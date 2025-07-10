2e1f3488936e4b0063d42e6db77d323187c83585
Alovvermind <wildner.mario@gmail.com>
2025-07-10 13:28:53 +0200

    Allow restzeit-inventur.onrender.com in vite preview config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './'  // relativ, damit Ressourcen auch bei Subpfaden geladen werden
+  preview: {
+    allowedHosts: ['restzeit-inventur.onrender.com']
+  }
 })
